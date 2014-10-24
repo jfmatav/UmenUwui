3da58363c46c72ca3330c1bebf3412c114908c2b
jfmatav <jfmatav@hotmail.com>
2014-10-24 15:13:50 -0600

    Modificación de vistas de snack para que muestren la soda en lugar de su id y combobox en el form para escoger la soda

--- app/helpers/snacks_helper.rb
+++ app/helpers/snacks_helper.rb
@@ -1,2 +1,11 @@
 module SnacksHelper
+
+  def idtostring(number)
+    idsToStrings = {"1" => "Odontología", "2" => "Derecho",
+                    "3" => "Economía", "4" => "Agronomía", 
+                    "5" => "Generales", "6" => "Educación",
+                    "7" => "Sociales", "8" => "Comedor"}
+    idsToStrings[number.to_s]
+  end
+
 end
