33ae73a37cb20835cb9089f461006575e0547e68
jfmatav <jfmatav@hotmail.com>
2014-10-29 13:37:30 -0600

    Agregados sábados a la lista de días de platos

--- app/helpers/platos_helper.rb
+++ app/helpers/platos_helper.rb
@@ -9,7 +9,7 @@ module PlatosHelper
   end
 
   def numtodia(number)
-    numToDia = {"1" => "Lunes", "2" => "Martes", "3" => "Miércoles", "4" => "Jueves", "5" => "Viernes"}
+    numToDia = {"1" => "Lunes", "2" => "Martes", "3" => "Miércoles", "4" => "Jueves", "5" => "Viernes", "6" => "Sábado"}
     numToDia[number.to_s]
   end
 
