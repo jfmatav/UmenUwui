bf538471b2eef5255b4e33c7abd6cfc830c0f032
jfmatav <jfmatav@hotmail.com>
2014-11-13 14:24:12 -0600

    Interfaz web actualizada para modelo Acompañamientos

--- app/helpers/acompanamientos_helper.rb
+++ app/helpers/acompanamientos_helper.rb
@@ -1,2 +1,16 @@
 module AcompanamientosHelper
+
+  def idtostring(number)
+    idsToStrings = {"1" => "Odontología", "2" => "Derecho",
+                    "3" => "Economía", "4" => "Agronomía", 
+                    "5" => "Generales", "6" => "Educación",
+                    "7" => "Sociales", "8" => "Comedor"}
+    idsToStrings[number.to_s]
+  end
+
+  def numtodia(number)
+    numToDia = {"1" => "Lunes", "2" => "Martes", "3" => "Miércoles", "4" => "Jueves", "5" => "Viernes", "6" => "Sábado"}
+    numToDia[number.to_s]
+  end
+
 end
